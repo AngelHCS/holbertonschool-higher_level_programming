53575959afddd5f6a6ff81a1439e83df977111a2
AngelHCS <8990holbertonstudents.com>
2024-05-09 16:59:04 -0400

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-
-
-
-
-
+def fizzbuzz(0):
+    for number in range(1, 101):
+        if number % 15 == 0:
+            print ("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print ("Fizz ", end="")
+        elif number % 5 == 0:
+            print ("Buzz ", end="")
+        else:
+            print (f"{number} ", end="")
