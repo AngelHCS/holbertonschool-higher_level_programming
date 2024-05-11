1fe2e377b962b13a5a70488ff85f683da61045d1
AngelHCS <8990holbertonstudents.com>
2024-05-10 22:22:31 -0400

    code shouldn't execute on import anymore?

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
+from add_0 import add  # wish i had this file to test output
 a = 1
 b = 2
-print({} + {}.format(a, b, add(a, b)))
+if b == 5:
+    print({} + {}.format(a, b, add(a, b)))
