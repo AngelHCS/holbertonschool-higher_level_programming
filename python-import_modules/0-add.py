e0818eb460d992472b40bbea6efea77f8f9a84fc
AngelHCS <8990holbertonstudents.com>
2024-05-10 22:25:08 -0400

    changing print, maybe issue was there?

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,5 +2,5 @@
 from add_0 import add  # wish i had this file to test output
 a = 1
 b = 2
-if b == 5:
-    print({} + {}.format(a, b, add(a, b)))
+if b == 5
+    print("{} + {} =[]".format(a, b, add(a, b)))
