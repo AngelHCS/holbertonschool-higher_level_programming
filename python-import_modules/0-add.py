fe6e679b086cfad8637d7707e00dab62c65868c4
AngelHCS <8990holbertonstudents.com>
2024-05-10 22:28:04 -0400

    Was using [] instead of {}, causing the output to just be [] instead of the desired value

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ from add_0 import add  # wish i had this file to test output
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{} + {} =[]".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
