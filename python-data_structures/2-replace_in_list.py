f1c4ff759114938ea385eb7d9b137bb91ff0a3b1
AngelHCS <8990holbertonstudents.com>
2024-05-13 23:17:24 -0400

    check 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 2-replace_in_list.py
-
-
 def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):  #i should use and more often
         my_list[idx] = element
