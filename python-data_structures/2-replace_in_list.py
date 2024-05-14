dd725a0b7e956756b138ffee033ca6fde3c71496
AngelHCS <8990holbertonstudents.com>
2024-05-13 23:18:51 -0400

    fixed pycodestyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):  #i should use and more often
+    if idx >= 0 and idx < len(my_list):  # i should use and more often
         my_list[idx] = element
     return (my_list)
