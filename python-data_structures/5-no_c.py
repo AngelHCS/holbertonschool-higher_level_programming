0062f2b2fbe1aa9f8a3212d4b54050674db5aa7a
AngelHCS <8990holbertonstudents.com>
2024-05-13 21:03:29 -0400

    made pycodestyle compliant

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(org_string):
-    string_noc =("")
+    string_noc = ("")
     for char in org_string:
         if char != 'c' and char != 'C':
             string_noc += char
