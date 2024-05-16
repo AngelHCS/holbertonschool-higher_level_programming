334241b680c4ec98e859579959f8641b5f626854
AngelHCS <8990holbertonstudents.com>
2024-05-16 09:48:34 -0400

    3rd times the charm

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -3,5 +3,4 @@ def raise_exception_msg(message="C is fun"):  # we are not using C but ok
     try:
         raise_expection_msg("C is fun")
     except NameError:
-        print("Name error")
         print(message)
