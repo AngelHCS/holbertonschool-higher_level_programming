275ffd9d47b8b3e847545dc15a5cf1bc506b67e4
AngelHCS <8990holbertonstudents.com>
2024-05-20 23:52:54 -0400

    added dollar sign to end of output

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,11 @@
 def say_my_name(first_name, last_name=""):
     # Check if first_name and last_name are strings otherwise, typeError
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
+        raise TypeError("first_name must be a string$")
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string$")
     # Print the name
     if last_name:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}$".format(first_name, last_name))
     else:
-        print("My name is", first_name)
+        print("My name is {} $" .format(first_name))
