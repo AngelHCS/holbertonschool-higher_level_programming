823f45fe178a78ed33a4bbc8270ba1d3c2637c00
AngelHCS <8990holbertonstudents.com>
2024-05-28 12:04:40 -0400

    supressed newline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,11 +4,11 @@
 def read_file(filename=""):
 
     """
-    Read the contents of a text file and print them to the console.
+    Read the contents of a text file and print them to stdout
 
     Args:
     filename (str): The name of the file to be read.
     """
 
     with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end ="")
