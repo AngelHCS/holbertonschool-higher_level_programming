978e6bf226ae19c2f566d0c77424eb25a85b5859
AngelHCS <8990holbertonstudents.com>
2024-05-28 16:49:20 -0400

    appends file, with the test from 2-main.py, only returns chars added as stdout

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """Append text to a file and return chars added"""
+
+    with open(filename, mode='a', encoding='utf-8') as file:
+        """Open where the file is using the encoding"""
+
+        file.write(text)
+        """Put the text in the file"""
+
+        return len(text)
+    """Return the chars added"""
