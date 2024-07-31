35c339fb17236843d811d1c519ff7f099aa57115
AngelHCS <8990holbertonstudents.com>
2024-07-30 20:21:15 -0400

    first 3 and starting a new directory in repo

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('header');
+
+const redHeader = document.querySelector('#red_header');
+
+redHeader.addEventListener('click', () =>
+  {
+    header.style.color = '#FF0000';
+  });
